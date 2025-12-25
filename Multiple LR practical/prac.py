d85f8312d28fb9441aa21d0eced61bb95fe1bf28
Satyajit-tech2006 <satyajitswain2006@gmail.com>
2025-12-25 20:24:04 +0530

    Remove unused script for loading and previewing economic index data

--- Multiple LR practical/prac.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df_index = pd.read_csv(
-    r"C:\Users\56ssw\OneDrive\Desktop\AI-ML\ML-Projects\Multiple LR practical\economic_index.csv"
-)
-print(df_index.head(10))
