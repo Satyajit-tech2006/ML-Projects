ae69c58790a77d41295844598349c8efae584ed7
Satyajit-tech2006 <satyajitswain2006@gmail.com>
2025-12-25 20:23:44 +0530

    Add initial implementation of economic index data loading and preview

---
+++ Multiple LR practical/prac.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df_index = pd.read_csv(
+    r"C:\Users\56ssw\OneDrive\Desktop\AI-ML\ML-Projects\Multiple LR practical\economic_index.csv"
+)
+print(df_index.head(10))
